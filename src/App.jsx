87f574a552b38ec3b1d4dd7bcd12cef1a35b2162
Anderson <asilvadesousa634@outlook.com>
2023-11-15 17:37:29 -0300

    adding some elements and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import "./App.css"
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <section className="container">
+      <div className="info">
+        <h1>Anderson da Silva</h1>
+        <h2>
+          <span className="highlighted">&#123;</span> front-end developer{" "}
+          <span className="highlighted">&#125;</span>
+        </h2>
+      </div>
+      <div className="social">
+        
+      </div>
+      <div className="about">
+        <p>
+          Born in Brazil and passionate about technology since early on, this is
+          me, always seeking to learn, practice, and share.
+        </p>
+      </div>
+    </section>
   )
 }
